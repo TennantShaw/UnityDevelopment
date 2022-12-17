f66ee66aea90f300abe2f21a679fab44c29ca8ea
Tennant S <emptyhuskgaming@gmail.com>
2022-12-17 16:45:55 -0500

    Tutorial Progression
    Completed 3.4

--- Unity Tutorial Programs/Create with Code 2/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Unity Tutorial Programs/Create with Code 2/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -12,6 +12,11 @@ public class PlayerController : MonoBehaviour
     public bool doubleJumpSkill = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +24,7 @@ public class PlayerController : MonoBehaviour
        playerRb = GetComponent<Rigidbody>();
        playerAnim = GetComponent<Animator>();
        Physics.gravity *= gravityModifier;
+       playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,6 +32,8 @@ public class PlayerController : MonoBehaviour
     {
         // check to see if player has pressed the space bar and player is on the ground
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) {
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false; 
             playerAnim.SetTrigger("Jump_trig");
@@ -40,8 +48,12 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground")) {
             isOnGround = true;
+            dirtParticle.Play();
             doubleJumpSkill = true;
         } else if (collision.gameObject.CompareTag("Obstacle")) {
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             gameOver = true;
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
